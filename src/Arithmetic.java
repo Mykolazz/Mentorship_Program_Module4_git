0eab1396381d17cce25c4e61418f823f5926e930
Mykola Protsiv <mykola_protsiv@epam.com>
2023-09-01 22:56:19 +0300

    addAdvanced method with 3 params added

--- src/Arithmetic.java
+++ src/Arithmetic.java
@@ -20,7 +20,12 @@ public class Arithmetic {
     }
 
     public int addAdvanced(int a, int b){
-        System.out.println("This is add method");
+        System.out.println("This is add method for 2 params");
         return a+b;
+    }
+
+    public int addAdvanced(int a, int b, int c){
+        System.out.println("This is add method for 3 params");
+        return a+b+c;
     }
 }
